5bf4c2eff41959354c431129efd5d3c4b0bce130
Laboratorio SL006 <sl006@unimet.local>
2024-09-24 10:01:08 -0400

    :sparkles: Listas con Arrays

--- Listas/src/listas/Main.java
+++ Listas/src/listas/Main.java
@@ -6,24 +6,27 @@ public class Main {
 
 
     public static void main(String[] args) {
-        Lista list = new Lista();
+        // Lista list = new Lista();
+        ListaArray list = new ListaArray(10);
         System.out.println("Lista Begin");
         for (int i = 0; i < 10; i++) {
             list.insertBegin(i);
         }
         list.print();
+        System.out.println("Secuencial");
+        list.printSecuencial();
         System.out.println("");
         System.out.println("Lista Final");
-        Lista listFinal = new Lista();
+        ListaArray listFinal = new ListaArray(10);
         for (int i = 0; i < 10; i++) {
             listFinal.insertFinal(i);
         }
         listFinal.print();
         System.out.println("");
-        System.out.println("Lista After Index"); 
+        /*System.out.println("Lista After Index"); 
         listFinal.deleteInIndex(4);
         listFinal.print();
-        System.out.println("");
+        System.out.println("");*/
     }
     
 }
